9e4465d65c16d4bdb2ca603dbdf73e83c3420331
Akib Maredia <akibmaredia@gmail.com>
2024-04-16 14:09:45 -0500

    [Fix #2] - Fix authentication issue

--- app/utils/AJAX.js
+++ app/utils/AJAX.js
@@ -1,7 +1,7 @@
 const Domain = 'http://127.0.0.1:8009';
 
 const SSOToken =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXJAZW1haWwuY29tIiwiaWQiOiJ1c2VyLmlkIiwibmF5bWUiOiJ1c2VyLm5hbWUiLCJpYXQiOjE2MDk5NzMyMzl9.7VSmwQSmkhkEAba8zgd1_05b8hQKovTZtVHxFKa3MNk';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXJAZW1haWwuY29tIiwiaWQiOiJ1c2VyLmlkIiwibmFtZSI6InVzZXIubmFtZSIsImlhdCI6MTYwOTk3MzIzOX0.1XNjKX3aOVdGKu0jr6HpxVc8ED19A5b-r1b3HF4kW2U';
 
 export function getSSOToken() {
   return SSOToken;
